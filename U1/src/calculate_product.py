1c802eb03b47dd21dae07a4aa1bc4d8dc48cdcbf
Jaap Pedersen <jaappedersen@hotmail.de>
2017-04-26 12:18:10 +0200

    adapted calculate prodcut

--- U1/src/calculate_product.py
+++ U1/src/calculate_product.py
@@ -1,21 +1,18 @@
-def calculate_product():
-    print('perform a multiplication with a value greater than 0 or terminate the program with 0')
-    x = int(input('Enter a number :'))
-    #check initial value
-    if x == 0:
-        raise ValueError('number has to be different from zero')
-    #set y=1 to enter while loop
-    y = 1
-    while y!= 0:
-        y = int(input('Enter a number greater or equal zero:'))
-        if y<0:
-            print('number has to be greater or equal zero')
-            y = int(input('Enter a number greater or equal zero'))
-        elif y != 0:
-            x*= y
-            print(x)
-        else:
-            break
+print('perform a multiplication with a value greater than 0 or terminate the program with 0')
+x = int(input('Enter a number :'))
+#check initial value
+if x == 0:
+    raise ValueError('number has to be different from zero')
+#set y=1 to enter while loop
+y = 1
+while y!= 0:
+    y = int(input('Enter a number, to terminate enter zero:'))
+    if y<0:
+        x *= y
+    elif y > 0:
+        x *= y
+        print(x)
+    else:
+        break
 
-    print('result of the product of all inserted number is: ',x)
-    return x
+print('result of the product of all inserted number is: ',x)
