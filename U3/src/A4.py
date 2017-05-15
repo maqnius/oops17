c6d97fb9e002f6ec1542a29492ac16a58745f15b
Stefaan <hessmann.stefaan@googlemail.com>
2017-05-16 00:36:45 +0200

    wieso geht das nicht?! FML

--- U3/src/A4.py
+++ U3/src/A4.py
@@ -1,25 +1,65 @@
 def create_matrix(n, m):
-    matrix = [[0]*m]*n
+    """
+    Create an empty n * m matrix
+    
+    Parameters
+    ----------
+    n : int
+    m : int
+    
+    Returns
+    -------
+    matrix : list of lists
+    
+    """
+    matrix = [[None]*m]*n
     return matrix
 
 def fill_matr(matrix, size):
+    """
+    Fill an empty matrix with inputs.
+    
+    Parametres
+    ----------
+    matrix : list of lists
+        empty matrix
+    size : int
+        size of the matrix
+    
+    Retruns
+    -------
+    matrix : list of lists
+        matrix filled with values
+    
+    """
     n = len(matrix[0])
     last_elem = 0
     while last_elem < size:
         i = last_elem // n
         j = last_elem % n
         print("enter another element")
-        print(i, j)
         new_elem = int(input())
         matrix[i][j] = new_elem
         print(matrix)
         last_elem += 1
     print("matrix is full")
-    print(matrix)
     return matrix
     
     
 def make_matrix():
+    """
+    Create a matrix. Size and elements are set by user via input().
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    matrix : list of lists
+        matrix with values
+    
+    """
     print("Create n*m Matrix")
     print("Enter matrix parameters:")
     print("n = ")
@@ -28,24 +68,42 @@ def make_matrix():
     m = int(input())
     size = n * m
     matrix = create_matrix(n, m)
-    print(matrix)
     matrix = fill_matr(matrix, size)
-    print(matrix)
+    return matrix
 
 
 def transpose_matrix(matrix):
+    """
+    Transpose a give n * m matrix.
+    
+    Parameters
+    ----------
+    matrix : list of lists
+        matrix that will be transposed
+    
+    Returns
+    -------
+    matrix_t : list of lists
+        transposed matrix
+    
+    """
     n = len(matrix)
     m = len(matrix[0])
-    print(n, m)
-    matrix_t = [[0] * n] * m
-    print(matrix_t)
+    matrix_t = [[None] * n] * m
     for j in range(m):
         for i in range(n):
+            print(matrix[i][j])
             matrix_t[j][i] = matrix[i][j]
     
     return matrix_t
     
     
-    
-matrix = [[1, 2, 3], [4, 5, 6]]
-print(transpose_matrix(matrix))
+
+if __name__ == '__main__':
+    # Aufgabe a)
+    matrix = make_matrix()
+    # Aufgabe b)
+    print(matrix)
+    # Aufgabe c)
+    matrix_t = transpose_matrix(matrix)
+    print(matrix_t)
