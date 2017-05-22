f5e47701ef6de1c9ea347ec5dcc3a3ae815c2e79
Mark Niehues <niehues.mark@gmail.com>
2017-05-22 16:31:36 +0200

    ready 99%

--- U4/src/A1.py
+++ U4/src/A1.py
@@ -3,7 +3,7 @@ Aufgabe 1
 """
 
 
-def rekursion(counter=0):
+def rekursion(counter=1, error_occured = False):
     """
     Calls itself until an error occurs.
     Prints the number of recursion-steps.
@@ -12,19 +12,22 @@ def rekursion(counter=0):
     ----------
     counter : int
         should not be set
+
+    error_occured : boolean
+		Determines if an error already occured
     
     Returns
     -------
     None
     """
-    error_occured = False
     counter += 1
     if not error_occured:
         try:
             rekursion(counter)
         except:
-            end = True
-            print("Nach {} Rekursionen ist die Rekursionstiefe erreicht".format(counter))
+            if not error_occured:
+            	print("Nach {} Rekursionen ist die Rekursionstiefe erreicht".format(counter))
+            	error_occured = True
 
 
 if __name__ == '__main__':
--- U4/src/A2.py
+++ U4/src/A2.py
@@ -1,24 +1,45 @@
 """
+Aufgabe 2
+Hessmann, Niehues, Pedersen
+
+Demonstrates the Towers of Hanoi using a recursive algorithm.
+
+Similar to solution:
 http://www.python-kurs.eu/tuerme_von_hanoi.php
 """
 
-def hanoi(n, source, helper, target):
+def hanoi(n, source, helper, target, rekursionstiefe = 0):
     if n > 0:
-        # Put thie print so somewhere, where it makes sense!
-        print("Tower 1: {} \t Tower 2: {} \t Tower 3: {}".format(source, helper, target))
         # move tower of size n - 1 to helper:
-        hanoi(n - 1, source, target, helper)
-        # move disk from source peg to target peg
-        if source:
-            target.append(source.pop())
-        # move tower of size n-1 from helper to target
-        hanoi(n - 1, helper, source, target)
+        hanoi(n - 1, source, target, helper, rekursionstiefe = rekursionstiefe + 1)
         
-n = int(input("How many discs?:\n"))
+        # move disk from source peg to target peg
+        if source[0]:
+            print("Moving {} from {} to {}".format(source[0][-1], source[1], target[1]))
+            target[0].append(source[0].pop())
 
-source = list(range(1,n+1)) # Creates list from 1 to n
-source = source[::-1]       # Invertes the order
+            # Parameters need to be seperated for printing
+            afg = ""
+            hlp = ""
+            zl = ""
+            for i in [source, target, helper]:
+                if i[1] == "Anfang":
+                    afg = i
+                elif i[1] == "Ziel":
+                    zl = i
+                elif i[1] == "Hilfsstab":
+                    hlp = i
+            print("{}: {}\t {}: {}\t {}: {}".format(afg[1], afg[0], hlp[1], hlp[0], zl[1], zl[0]))
 
-target = []
-helper = []
-hanoi(n,source,helper,target)
+        # move tower of size n-1 from helper to target
+        hanoi(n - 1, helper, source, target, rekursionstiefe = rekursionstiefe + 1)
+
+
+if __name__ == '__main__':
+    n = int(input("How many discs?:\n"))
+
+    source = list(range(1,n+1)) # Creates list from 1 to n
+    source = (source[::-1], "Anfang")       # Invertes the order
+    target = ([], "Ziel")
+    helper = ([], "Hilfsstab")
+    hanoi(n,source,helper,target)
