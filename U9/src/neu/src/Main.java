a59dc69ca1ac3687c6f01ca4b46744944ac72788
Mark Niehues <niehues.mark@gmail.com>
2017-06-27 15:13:29 +0200

    not tested

---
+++ U9/src/neu/src/Main.java
@@ -0,0 +1,10 @@
+package neu.src;
+
+/**
+ * Created by mark on 27.06.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ U9/src/neu/src/Street.java
@@ -0,0 +1,97 @@
+package neu.src;
+
+/**
+ * Created by mark on 27.06.17.
+ */
+public class Street {
+    private House[] houses;
+    private int MaxHouses;
+
+    /**
+     * Initialize empty Street with space for MaxHouse houses
+     * @param buildYear The year the Street was build
+     * @param MaxHouses Maximum number houses
+     */
+    public Street(int MaxHouses) {
+        houses = new House[MaxHouses];
+        this.MaxHouses = MaxHouses;
+    }
+
+    /**
+     * Several people want to move in
+     * @param numberOfPeople
+     * @return
+     */
+    public boolean moveIn(int numberOfPeople){
+        for(int i = 0; i < numberOfPeople; i++){
+            if(!moveIn()){
+                return false;
+            }
+        }
+        // Everyone could move in
+        return true;
+    }
+
+    /**
+     * Someone wants to move in
+     * @return
+     */
+    public boolean moveIn(){
+        // Try to find free Slot in existing House
+        for(House h: houses) {
+            if(!h.equals(null) && h.moveInHouse()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean buildNewHouse(int buildYear, int totalRooms, int lifeSpan){
+        // Look for free slot
+        for(House h: houses){
+            if(h.equals(null)){
+                h = new House(buildYear, totalRooms, lifeSpan);
+                return true;
+            }
+        }
+        // Street full
+        return false;
+    }
+
+    /**
+     * Looks for empty buildings that are over due
+     */
+    public void cleanStreet(int currentYear){
+        for(House h: houses) {
+            if(!h.equals(null) && h.isEmpty() && h.isOverdue(currentYear)){
+                h = null;
+            }
+        }
+    }
+
+    public void printStreet(int currentYear){
+        System.out.println("");
+        for (House h: houses) {
+            if (!h.equals()) {
+                System.out.print(street.getHouses()[property].getOccupiedRooms());
+            }
+            if (street.getHouses()[property] == null) {
+                System.out.print("X");
+            }
+            System.out.print("   ");
+        }
+        System.out.println("");
+        for (int property = 0; property < size; property++) {
+            System.out.print("----");
+        }
+        System.out.println("");
+        for (int property = 0; property < size; property++) {
+            System.out.print(" - -");
+        }
+        System.out.println("");
+        for (int property = 0; property < size; property++) {
+            System.out.print("----");
+        }
+    }
+}
