26a0503a86688259093fddfdba76706c70685ca2
Mark Niehues <niehues.mark@gmail.com>
2017-06-27 13:33:43 +0200

    Changed House a bit

--- U9/src/House.java
+++ U9/src/House.java
@@ -1,47 +1,88 @@
-public class House{
-  // Attributes
-  int buildYear;
-  int lifeSpan;
-  int totalRooms;
-  int occupiedRooms;
-  int houseNumber;
-  boolean canBreak;
-  // Constructor
-  public House(int houseNumber, int buildYear){
-    this.buildYear = buildYear;
-    this.lifeSpan = 60;
-    this.totalRooms = 5;
-    this.occupiedRooms = 0;
-    this.houseNumber = houseNumber;
-    this.canBreak = false;
-  }
-  // Methods
-  public int getTotalRooms(){
-    return this.totalRooms;
-  }
-  public int getOccupiedRooms(){
-    return this.occupiedRooms;
-  }
-  public int getUnoccupiedRooms(){
-    return this.totalRooms - this.occupiedRooms;
-  }
-  public void updateCanBreak(int theYear){
-    if (this.buildYear-theYear < 0 & this.occupiedRooms == 0){
-      this.canBreak = true;
+public class House {
+    // Attributes
+    private int buildYear;
+    private int lifeSpan;
+    private int totalRooms;
+    private int occupiedRooms;
+    private int houseNumber;
+    boolean canBreak;
+
+    /**
+     * Constructor for the House Class with individual lifespan and number of rooms
+     * @param houseNumber Adress
+     * @param buildYear Year in which the house was build
+     * @param totalRooms Number of rooms in the house
+     * @param lifeSpan Lifespan of the building after which it gets destroyed
+     */
+    public House(int houseNumber, int buildYear, int totalRooms, int lifeSpan){
+        this.buildYear = buildYear;
+        this.houseNumber = houseNumber;
+        this.totalRooms = totalRooms;
+        this.lifeSpan = lifeSpan;
+
+        occupiedRooms = 0;
+        canBreak = false;
     }
-  }
-  public void unoccupyRoom(){
-    if (this.occupiedRooms > 0){
-      this.occupiedRooms--;
+
+
+    public House(int houseNumber, int buildYear) {
+        this.buildYear = buildYear;
+        this.houseNumber = houseNumber;
+        lifeSpan = 60;
+        totalRooms = 5;
+        occupiedRooms = 0;
+        canBreak = false;
     }
-    else {System.out.println("there is no occupied room in this house");
+
+    public void setOccupiedRooms(int occupiedRooms) {
+        this.occupiedRooms = occupiedRooms;
     }
-  }
-  public void occupyRoom(){
-    if (this.occupiedRooms < this.totalRooms){
-      this.occupiedRooms++;
+
+    public int getBuildYear() {
+        return buildYear;
     }
-    else {System.out.println("all rooms are allready occupied!");}
-  }
+
+    public int getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public int getOccupiedRooms() {
+        return occupiedRooms;
+    }
+
+    public int getUnoccupiedRooms() {
+        return totalRooms - occupiedRooms;
+    }
+
+    public void updateCanBreak(int theYear) {
+        if (buildYear - theYear < 0 & occupiedRooms == 0) {
+            canBreak = true;
+        }
+    }
+
+    public void unoccupyRoom() {
+        if (occupiedRooms > 0) {
+            occupiedRooms--;
+        } else {
+            System.out.println("there is no occupied room in this house");
+        }
+    }
+
+    public void occupyRoom() {
+        if (occupiedRooms < totalRooms) {
+            occupiedRooms++;
+        } else {
+            System.out.println("all rooms are allready occupied!");
+        }
+    }
 
 }
